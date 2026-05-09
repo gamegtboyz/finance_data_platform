bb7fd73bac148a4c2474763f58ecb915db9d4597
Asa Bejraputra <gamegame.gtboyz@gmail.com>
2026-05-10 01:28:20 +0700

    fix the sqlfluff **kwargs incompatability

--- finance_dbt/macros/sqlfluff_mocks.sql
+++ finance_dbt/macros/sqlfluff_mocks.sql
@@ -1,4 +1,4 @@
-{% macro config(**kwargs) %}{% endmacro %}
+{% macro config(materialized='', unique_key='', dist='', sort='') %}{% endmacro %}
 {% macro ref(model_name) %}{{ model_name }}{% endmacro %}
 {% macro source(source_name, table_name) %}{{ source_name }}_{{ table_name }}{% endmacro %}
 {% macro is_incremental() %}false{% endmacro %}
