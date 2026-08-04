ea9a41e7c27e0b19d8a28b6588c86a6c88a243e9
Asa Bejraputra <gamegame.gtboyz@gmail.com>
2026-08-04 07:12:44 +0700

    add news_sentiement staging dbt models

---
+++ finance_dbt/models/staging/stg_news_sentiment.sql
@@ -0,0 +1,11 @@
+{{ config(materialized='view') }}
+
+SELECT
+    symbol,
+    date,
+    headline,
+    sentiment_score,
+    source,
+    url
+    GETDATE() as loaded_at
+FROM {{ source('raw', 'news_sentiment')}}
